f2bbb9b6e4e6cbb08f4d0d002b061b38d55ffb40
Aziz Kosber <a.kosber@gmail.com>
2018-03-14 17:38:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -19,6 +19,6 @@ Object.assign(course, {start_date: "2017-01-01"})
 
 Object.assign({}, course, {start_date: "2017-01-01"})
 
-delete course["title"]
+delete course["Title"]
 
 Object.assign({}, course, {description: "a kick-ass introduction to JS"})
